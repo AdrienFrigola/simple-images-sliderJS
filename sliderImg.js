46ed26fddb3e9d830c9135803eea60250531531b
ADRIEN FRIGOLA <133056050+AdrienFrigola@users.noreply.github.com>
2024-02-22 03:41:42 +0200

    Update sliderImg.js

--- sliderImg.js
+++ sliderImg.js
@@ -1,45 +1,57 @@
+// Select all images with the class "slides"
 const slides = document.querySelectorAll(".slides img");
+
+// Initialize the slide index and interval ID
 let slideIndex = 0;
 let intervalId = null;
 
+// Event listener for DOMContentLoaded to initialize the slider when content is loaded
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
+// Function to initialize the slider
 function initializeSlider(){
+    // Check if there are slides
     if(slides.length > 0){
+        // Add the "displaySlide" class to the first slide
         slides[slideIndex].classList.add("displaySlide");
-        intervalId = setInterval(nextSlide, 5000);
-        
-    }
-
-    
+        // Start the interval to move to the next slide every 5 seconds
+        intervalId = setInterval(nextSlide, 5000);   
+    }   
 }
 
+// Function to display a slide at the specified index
 function showSlide(index){
-
+    // If the index is greater than or equal to the number of slides, go back to the first slide
     if(index >= slides.length){
         slideIndex = 0;
-    }else if(index < 0){
-        slideIndex = slides.length -1;
+    } else if(index < 0){ // If the index is less than zero, move to the last slide
+        slideIndex = slides.length - 1;
     }
 
+    // Remove the "displaySlide" class from all slides
     slides.forEach(slide =>{
-    slide.classList.remove("displaySlide");
+        slide.classList.remove("displaySlide");
     });
+
+    // Add the "displaySlide" class to the current slide
     slides[slideIndex].classList.add("displaySlide");
-    
-
-
 }
 
+// Function to move to the previous slide
 function prevSlide(){
+    // Stop the interval
     clearInterval(intervalId);
-    slideIndex --;
+    // Decrement the slide index and display the corresponding slide
+    slideIndex--;
     showSlide(slideIndex);
 }
 
+// Function to move to the next slide
 function nextSlide(){
-    slideIndex ++;
+    // Increment the slide index and display the corresponding slide
+    slideIndex++;
     showSlide(slideIndex);
+    // Stop the current interval and start a new one
     clearInterval(intervalId);
     intervalId = setInterval(nextSlide, 5000);
 }
